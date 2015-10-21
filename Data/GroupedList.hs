fa0bb5eef30ce705cd86bafc2c86d23f2a51243b
Daniel Díaz <dhelta.diaz@gmail.com>
2015-10-21 18:59:20 -0400

    Compatibility.

--- Data/GroupedList.hs
+++ Data/GroupedList.hs
@@ -71,7 +71,7 @@ import qualified GHC.Exts as GHC
 -- COMPATIBILITY
 
 #if !MIN_VERSION_base(4,8,0)
-import Control.Applicative (Applicative (..))
+import Control.Applicative (Applicative (..), (<$>))
 import Data.Foldable (Foldable (foldMap))
 import Data.Monoid (Monoid (..))
 #endif
@@ -216,8 +216,10 @@ map f (Grouped gs) = Grouped $
 
 instance Foldable Grouped where
   foldMap f (Grouped gs) = foldMap (foldMap f) gs
+#if MIN_VERSION_base(4,8,0)
   length (Grouped gs) = foldl' (+) 0 $ fmap length gs
   null (Grouped gs) = null gs
+#endif
 
 instance Show a => Show (Grouped a) where
   show = show . toList
